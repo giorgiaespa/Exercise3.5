5f9b8e27d8bca48903772a2b0370ca29a79dfff3
gio <giorgiaespa2.0@gmail.com>
2023-10-15 22:04:16 +0200

    Initial commit

---
+++ Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+        Rettangolo rettangolo = new Rettangolo(8,9.12);
+        rettangolo.calcolaArea();
+
+        Triangolo triangolo = new Triangolo(7, 13);
+        triangolo.calcolaArea();
+    }
+}
